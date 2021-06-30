e4726bcb07f564847c9eab9d1abd6c75b825dc4a
Christopher Mullins <cmullins318@gmail.com>
2021-06-30 15:22:23 -0700

    finished

--- src/test/message.js
+++ src/test/message.js
@@ -26,39 +26,119 @@ after((done) => {
 })
 
 
-describe('Message API endpoints', () => {
+describe("Message API endpoints", () => {
     beforeEach((done) => {
-        // TODO: add any beforeEach code here
-        done()
-    })
-
+      // TODO: add any beforeEach code here
+      const sampleUser = new User({
+        _id: SAMPLE_USER_ID,
+        username: "tristann3",
+        password: "camelCased",
+      });
+      sampleUser
+        .save()
+        .then(() => {
+          return chai.request(app).post("/messages/").send({
+            _id: SAMPLE_MESSAGE_ID,
+            title: "anotherMessage",
+            body: "anotherBody",
+            author: SAMPLE_USER_ID,
+          });
+        })
+        .then(() => {
+          done();
+        });
+    });
+  
     afterEach((done) => {
-        // TODO: add any afterEach code here
-        done()
-    })
-
-    it('should load all messages', (done) => {
-        // TODO: Complete this
-        done()
-    })
-
-    it('should get one specific message', (done) => {
-        // TODO: Complete this
-        done()
-    })
-
-    it('should post a new message', (done) => {
-        // TODO: Complete this
-        done()
-    })
-
-    it('should update a message', (done) => {
-        // TODO: Complete this
-        done()
-    })
-
-    it('should delete a message', (done) => {
-        // TODO: Complete this
-        done()
-    })
-})
+      Message.deleteMany({ title: { $ne: "" } }).then(() => {
+        return User.deleteMany({ username: { $ne: "" } }).then(() => {
+          done();
+        });
+      });
+      // done();
+    });
+  
+    it("should load all messages", (done) => {
+      // TODO: Complete this
+      chai
+        .request(app)
+        .get("/messages")
+        .end((error, response) => {
+          if (error) done(error);
+          expect(response).to.have.status(200);
+          done();
+        });
+    });
+  
+    it("should get one specific message", (done) => {
+      console.log(SAMPLE_MESSAGE_ID);
+      chai
+        .request(app)
+        .get(`/messages/${SAMPLE_MESSAGE_ID}`)
+        .end((error, response) => {
+          if (error) done(error);
+          expect(response).to.have.status(200);
+          expect(response.body).to.be.an("object");
+          expect(response.body._id).to.equal(SAMPLE_MESSAGE_ID);
+          done();
+        });
+    });
+  
+    it("should post a new message", (done) => {
+      chai
+        .request(app)
+        .post("/messages")
+        .send({
+          title: "anotherMessage",
+          body: "anotherBody",
+          author: "60189f975561f51e8ef4e9df",
+        })
+        .end((error, response) => {
+          if (error) {
+            done(error);
+          }
+          expect(response).to.have.status(200);
+          expect(response);
+          done();
+        });
+    });
+  
+    it("should update a message", (done) => {
+      chai
+        .request(app)
+        .put(`/messages/${SAMPLE_MESSAGE_ID}`)
+        .send({ title: "anotherTitle" })
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res.body.message).to.be.an("object");
+          expect(res.body.message).to.have.property("title", "anotherTitle");
+  
+          //check that message is actually inserted into database
+          Message.findOne({ title: "anotherTitle" }).then((message) => {
+            expect(message).to.be.an("object");
+            done();
+          });
+        });
+    });
+  
+    it("should delete a message", (done) => {
+      chai
+        .request(app)
+        .delete(`/messages/${SAMPLE_MESSAGE_ID}`)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res.body.message).to.equal("Successfully deleted.");
+          expect(res.body._id).to.equal(SAMPLE_MESSAGE_ID);
+  
+          //check that the user is actually deleted from the database
+          Message.findOne({ title: "anotherMessage" }).then((message) => {
+            expect(message).to.equal(null);
+            done();
+          });
+        });
+    });
+  });
